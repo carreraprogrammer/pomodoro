72edea58d31adb2fcf175d8b9888e2fbea5d0807
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-14 00:08:16 -0500

    Check points 9 and 10

--- src/Components/Pomodoro.js
+++ src/Components/Pomodoro.js
@@ -1,6 +1,11 @@
 const Pomodoro = () => {
     return (
+      <div id='pomodoro-container'>
         <h1 id='timer-label'>Pomodoro</h1>
+        <h2 id='time-left'>25:00</h2>
+        <button id='start_stop'>Start</button>
+        <button id='reset'>Reset</button>
+      </div>
     )
 }
 
