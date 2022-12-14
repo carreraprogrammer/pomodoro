8c1c532cfdf0c88a3ae9c5f45eef8cb30ef4ed06
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-12-14 00:25:57 -0500

    Wrap App on provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Pomodoro from "./Components/Pomodoro";
 import UpdateBreak from "./Components/UpdateBreak";
 import UpdateTime from "./Components/UpdateTime";
+import { Provider } from 'react-redux';
+import store from "./Redux/store";
 import './Styles/App.css'
 
 const App = () => {
     return(
+      <Provider store={store}>
         <div id='app-container'>
           <UpdateBreak />
           <UpdateTime/>
           <Pomodoro />
         </div>
+      </Provider>
     )
 }
 
